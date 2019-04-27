90e7d9c618ca9e72bcf5c503015e73d5a2be9f3c
Wawan Riswanto <riswantowawan85@gmail.com>
2019-04-27 14:39:32 +0700

    first commit

---
+++ app/src/main/java/com/example/timer/TimerExpiredReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.timer
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.timer.util.NotificationUtil
+import com.example.timer.util.PrefUtil
+
+class TimerExpiredReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        NotificationUtil.showTimerExpired(context)
+
+        PrefUtil.setTimerState(MainActivity.TimerState.Stopped, context)
+        PrefUtil.setAlarmSetTime(0, context)
+    }
+}
